75f0ee2d67e21a1221a0be56168a4600a701e17c
Dan Lubbers <lubbers.dan@gmail.com>
2018-04-17 16:09:15 -0600

    added alter tables in sql seed file

--- houser/db/houser_seed.sql
+++ houser/db/houser_seed.sql
@@ -1,3 +1,4 @@
+-- Create table template
 CREATE TABLE houser (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30),
@@ -10,6 +11,7 @@ CREATE TABLE houser (
     rent DECIMAL
 )
 
+-- Insert data into Table
 insert into houser (name, address, city, state, zip, img, mortgage, rent) 
 values ('Triplex', '4567 S Road st', 'Orem', 'UT', 41312, 'https://www.utahrealestate.com/site/img/common/marquee-05.jpg', 250000.00, 1500.00);
 
@@ -18,3 +20,7 @@ values ('Dummy House', '4567 Dummy Road', 'Dummy', 'UT', 41312, 'http://www.dumm
 
 insert into houser (name, address, city, state, zip, img, mortgage, rent) 
 values ('Lego House', '1234 N Lego St', 'Legoville', 'NY', 40241, 'http://hannesdorfmann.com/images/legohouse/legohouse.jpg', 50.00, 5.00);
+
+-- Alter table test
+alter table houser
+add test_column varchar(20);
